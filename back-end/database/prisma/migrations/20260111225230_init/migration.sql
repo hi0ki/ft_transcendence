e390e5a665411dd79faba3c07a8a132b29b3c5ae
el mehdi el ansary <ansarimehdi518@gmail.com>
2026-01-11 23:55:10 +0100

    create tables (users, posts)

---
+++ back-end/database/prisma/migrations/20260111225230_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "post_type" AS ENUM ('help', 'resource', 'meme');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tables" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "type" "post_type" NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "tables_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
