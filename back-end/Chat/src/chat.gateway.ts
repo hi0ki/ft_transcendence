0075cc8c7ff3bcc066d7f9711c1cda9fb6f15dd3
Hafssa ANBR <hafssa.2017m5@gmail.com>
2026-01-25 19:28:22 +0100

    first steps

--- back-end/Chat/src/chat.gateway.ts
+++ back-end/Chat/src/chat.gateway.ts
@@ -1,7 +1,13 @@
-import { WebSocketGateway } from "@nestjs/websockets";
-
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from 'socket.io';
 @WebSocketGateway(80, {namespace: 'chat'})
 
 export class ChatGateway{
+    @WebSocketServer()
+    server: Server;
+    @SubscribeMessage('message')//mot message liee au handleSubmitNewMessage in chat-socket.js n src_client
+    handleMessage(@MessageBody() message: string): void{
+        this.server.emit('message', message);
+    }
 
 }
