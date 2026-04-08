c6f47cff6638ff69d148ecf13c2735b3bb28f5e6
hi0ki <ansarimehdi518@gmail.com>
2026-04-08 22:46:06 +0100

    adding dashboard config to the json file

--- backend/auth/src/metrics/metrics.service.ts
+++ backend/auth/src/metrics/metrics.service.ts
@@ -28,7 +28,7 @@ export const httpRequestDuration = new client.Histogram({
     name: 'http_request_duration_seconds',
     help: 'HTTP request duration',
     labelNames: ['user_role', 'method', 'route', 'status', 'BodySize'],
-    buckets: [0.1, 0.5, 1, 2.5, 5, 10],
+    buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5],
 });
 
 export const activeUsers = new client.Gauge({
